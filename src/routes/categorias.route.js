6ce13746bc877261dc59517eeb847157cbae0531
Emmanuel <manumtz20@gmail.com>
2024-01-23 15:10:52 -0600

    routes/index.js importar enrutado de categorias ya ceado en routes/categorias.rute.js

--- src/routes/categorias.route.js
+++ src/routes/categorias.route.js
@@ -1,4 +1,13 @@
-router.get('/categorias/id', getCategoriasByIdController)
-router.get('/categorias', getCategoriasController)
-router.post('/categorias', postCategoriasController)
-router.delete('/categorias/:id', deleteCategoriasController)
+const { Router } = require("express");
+
+//aqui se importan los controllers
+
+// creamos el enrutado para categorias
+const categoriesRouter = Router();
+
+categoriesRouter.get("/id", getCategoriasByIdController);
+categoriesRouter.get(getCategoriasController);
+categoriesRouter.post(postCategoriasController);
+categoriesRouter.delete("/:id", deleteCategoriasController);
+
+module.exports = categoriesRouter;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,10 @@
-const { Router } = require('express');
+const { Router } = require("express");
 
 // Importar todos los routers;
+const categoriesRouter = require("./categorias.route");
 // Ejemplo: const authRouter = require('./auth.js');
 const router = Router();
+
+router.use("/categories", categoriesRouter);
 
 module.exports = router;
