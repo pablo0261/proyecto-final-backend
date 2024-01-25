2c476f52195909285234bd239bc616fa4171fb1b
Freyddder <freyddercardenas22@gmail.com>
2024-01-24 21:08:53 -0300

    Ruta categories_options creada.

--- src/routes/categoriesRouter.js
+++ src/routes/categoriesRouter.js
@@ -1,12 +1,31 @@
-const { Router } = require('express');
+const { Router } = require("express");
 
 // controllers
-const getCategories = require('../controllers/categories/getCategories.controller');
-const postCategories = require('../controllers/categories/postCategories.controller');
+const getCategories = require("../controllers/categories/getCategories.controller");
+const getCategoriesOptions = require("../controllers/categories_options/getCategoriesOptions.controller");
+const postCategories = require("../controllers/categories/postCategories.controller");
 
 const categoriesRouter = Router();
 
-categoriesRouter.get('/categories', getCategories);
-categoriesRouter.post('/categories', postCategories);
+categoriesRouter.get("/categories", getCategories);
+categoriesRouter.get("/categoriesoptions", getCategoriesOptions);
+/**
+ * @swagger
+ * /categoriesoptions:
+ *   get:
+ *     summary: Filtrar las opciones de las categorias.
+ *     tags:
+ *       - Categorias
+ *     responses:
+ *       200:
+ *         description: >
+ *           Obtiene un objeto con la propiedad "categorias opcion", que tiene como valor un objeto con una propiedad:
+ *           data: Un array de objetos con todas las opciones que coinciden con el tipo de categoria especificado.
+ *       404:
+ *         description: No hay datos en categories_options.
+ *       500:
+ *         description: Error interno del servidor.
+ */
+categoriesRouter.post("/categories", postCategories);
 
 module.exports = categoriesRouter;
---
+++ src/services/categories/categories_options/getCategoriesOption.service.js
@@ -0,0 +1,8 @@
+const { CategoriesOptions } = require("../../../db");
+
+const getCategoriesOption = async () => {
+  const categoriesOptionsData = await CategoriesOptions.findAll();
+  return categoriesOptionsData;
+};
+
+module.exports = getCategoriesOption
