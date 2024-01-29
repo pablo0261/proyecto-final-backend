6b5b23fe520d5f283eeab61df339f354ea565195
Emmanuel <manumtz20@gmail.com>
2024-01-28 23:28:58 -0600

    nuevo archivo service/loginPeople

---
+++ src/services/people/loginPeople.service.js
@@ -0,0 +1,16 @@
+const { People } = require('../../db');
+const { getPeopleByIdService } = require('./getPeopleById.service');
+
+const loginPeopleService = async (email, password) => {
+  const people = await People.findOne({
+    where: { email, password },
+  });
+
+  if (people) {
+    people.logged = true;
+    await people.save();
+    return await getPeopleByIdService(people.idPeople);
+  }
+};
+
+module.exports = loginPeopleService;
