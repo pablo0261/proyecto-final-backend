dc97a8e4189fcbd07354b4f21bad5a635ef4089d
Emmanuel <manumtz20@gmail.com>
2024-01-29 14:25:27 -0600

    loginPopleService ahora retorna el id y actualiza la tabla people logins

--- src/services/people/loginPeople.service.js
+++ src/services/people/loginPeople.service.js
@@ -1,5 +1,5 @@
-const { People } = require('../../db');
-const { getPeopleByIdService } = require('./getPeopleById.service');
+const { People, People_logins } = require('../../db');
+const { v4: uuidv4 } = require('uuid');
 
 const loginPeopleService = async (email, password) => {
   const people = await People.findOne({
@@ -7,9 +7,17 @@ const loginPeopleService = async (email, password) => {
   });
 
   if (people) {
+    const currentDate = new Date();
+    const idPeople = people.idPeople;
+
     people.logged = true;
     await people.save();
-    return people.idPeople;
+    await People_logins.create({
+      id: uuidv4(),
+      idPeople,
+      loginDate: currentDate,
+    });
+    return idPeople;
   }
 };
 
