69e842b236b833a95fce42491e066f49b61978be
Freyddder <freyddercardenas22@gmail.com>
2024-01-29 23:02:21 -0300

    prueba de eliminado de rutas defectuosas

--- src/controllers/opportunities/postOpportunities.controller.js
+++ src/controllers/opportunities/postOpportunities.controller.js
@@ -1,46 +1,46 @@
-const { createOpportunity } = require("../../services/opportunities/opportunities.service");
+// const { createOpportunity } = require("../../services/opportunities/opportunities.service");
 
-const postOpportunities = async (req, res) => {
-  try {
-    const {
-      idCustomer,
-      idProvider,
-      dateQuery,
-      idService,
-      price,
-      dateHiring,
-      dateCanceled,
-      idCanceler,
-      dateServiceEnd,
-      ratingProvider,
-      ratingCustomer,
-      reviewCustomer,
-      reviewProvider,
-    } = req.body;
+// const postOpportunities = async (req, res) => {
+//   try {
+//     const {
+//       idCustomer,
+//       idProvider,
+//       dateQuery,
+//       idService,
+//       price,
+//       dateHiring,
+//       dateCanceled,
+//       idCanceler,
+//       dateServiceEnd,
+//       ratingProvider,
+//       ratingCustomer,
+//       reviewCustomer,
+//       reviewProvider,
+//     } = req.body;
 
-    const opportunityData = {
-      idCustomer,
-      idProvider,
-      dateQuery,
-      idService,
-      price,
-      dateHiring,
-      dateCanceled,
-      idCanceler,
-      dateServiceEnd,
-      ratingProvider,
-      ratingCustomer,
-      reviewCustomer,
-      reviewProvider,
-    }; 
+//     const opportunityData = {
+//       idCustomer,
+//       idProvider,
+//       dateQuery,
+//       idService,
+//       price,
+//       dateHiring,
+//       dateCanceled,
+//       idCanceler,
+//       dateServiceEnd,
+//       ratingProvider,
+//       ratingCustomer,
+//       reviewCustomer,
+//       reviewProvider,
+//     }; 
 
-    const createdOpportunity = await createOpportunity(opportunityData);
+//     const createdOpportunity = await createOpportunity(opportunityData);
 
-    res.status(201).json(createdOpportunity);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Error al procesar la solicitud" });
-  }
-};
+//     res.status(201).json(createdOpportunity);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ error: "Error al procesar la solicitud" });
+//   }
+// };
 
-module.exports = postOpportunities;
+// module.exports = postOpportunities;
