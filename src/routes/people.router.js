f2b42245b8080667ccfe85628d1a0757983ce0f8
LinxInformatica <diegolepore01@gmail.com>
2024-01-31 07:38:32 -0300

    agrega ordenamiento por cualquier campo de la tabla People

--- src/routes/people.router.js
+++ src/routes/people.router.js
@@ -2,12 +2,12 @@ const { Router } = require('express');
 
 // controllers
 const { getPeopleController } = require("../controllers/people/getPeople.controller.js");
-const { getPeopleByIdController } = require('../controllers/people/getPeopleById.controller.js');
+// const { getPeopleByIdController } = require('../controllers/people/getPeopleById.controller.js');
 const { postPeopleController } = require('../controllers/people/postPeople.controller.js');
 
 const peopleRouter = Router();
 
-peopleRouter.get('/people/:id', getPeopleByIdController);
+//peopleRouter.get('/people/:id', getPeopleByIdController);
 /**
  * @swagger
  * /people/{id}:
