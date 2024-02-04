d0f8f3ec62126890277957eb07bad0d1e1133850
LinxInformatica <diegolepore01@gmail.com>
2024-02-04 20:06:41 -0300

    arregla updateAges

--- src/utils/updateAges.js
+++ src/utils/updateAges.js
@@ -1,19 +1,28 @@
-const updateAges = async (instance) => {
+const {conn,People}=require('../db')
+
+const updateAges = async (params) => {
     const today = new Date();
-    const birthDateStr = instance.getDataValue('birthDate');
 
-    const birthDate = new Date(birthDateStr);
+    const allPeople = await People.findAll({
+        where: conn.literal(`EXTRACT( DAY FROM "birthDate") = ${today.getDate()} AND EXTRACT ( MONTH FROM "birthDate") = ${today.getMonth() + 1}`),
+    });
+    await Promise.all(allPeople.map(async (person) => {
 
-    if (birthDate instanceof Date) {
-        let age = today.getFullYear() - birthDate.getFullYear();
-        if (
-            today.getMonth() < birthDate.getMonth() ||
-            (today.getMonth() === birthDate.getMonth() && today.getDate() < birthDate.getDate())
-        ) {
-            age--; // Resta 1 año si aún no ha cumplido años este año
+        const birthDateStr = instance.getDataValue('birthDate');
+
+        const birthDate = new Date(birthDateStr);
+
+        if (birthDate instanceof Date) {
+            let age = today.getFullYear() - birthDate.getFullYear();
+            if (
+                today.getMonth() < birthDate.getMonth() ||
+                (today.getMonth() === birthDate.getMonth() && today.getDate() < birthDate.getDate())
+            ) {
+                age--; // Resta 1 año si aún no ha cumplido años este año
+            }
+            await instance.update({ age });
+
         }
-        await instance.update({ age });
-    }
-};
-
+    }))
+}
 module.exports = updateAges;
