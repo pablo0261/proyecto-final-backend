a8e142575e0767e84ac5194e62fb9bad679749da
Emmanuel <manumtz20@gmail.com>
2024-02-07 18:09:47 -0600

    correccion de conflicto del borrado logico en postPeopleOptions

--- src/services/people_options/deletePeopleOptions.service.js
+++ src/services/people_options/deletePeopleOptions.service.js
@@ -1,16 +1,16 @@
 const { People_options } = require('../../db');
+const { getPeopleService } = require('../people/getPeople.service');
 
 const deletePeopleOptionsService = async ({ idPeople, idOption }) => {
-  const [numOfRowsDeleted] = await People_options.update(
-    { isDeleted: true },
-    { where: { idPeople, idOption } }
-  );
+  const [numOfRowsDeleted] = await People_options.update({ isDeleted: true }, { where: { idPeople, idOption } });
 
   if (numOfRowsDeleted === 0) {
     return { status: 404, response: 'No hay registros' };
   }
 
-  return { status: 200, response: 'borrado carrectamente' };
+  const people = await getPeopleService({ idPeople });
+
+  return people ? { status: 200, response: people } : { status: 500, response: 'Error al devolver datos' };
 };
 
 module.exports = deletePeopleOptionsService;
