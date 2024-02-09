10acc565d715dfe3a20f5087e2169abe6941256a
tiago1820 <tiago.zdo@gmail.com>
2024-02-09 18:37:40 -0300

    Manejo de errores de leginPeopleController

--- src/controllers/logins/loginPeople.controller.js
+++ src/controllers/logins/loginPeople.controller.js
@@ -1,19 +1,32 @@
 const loginPeopleService = require('../../services/logins/loginPeople.service');
 const { getPeopleService } = require('../../services/people/getPeople.service');
+const { authenticateUser } = require("../../utils/authenticateUser.util");
 
 const loginPeopleController = async (req, res) => {
-  const { email, password } = req.body;
-  
+    const { email, password } = req.body;
 
-  if (!email) return res.status(400).json({ error: 'Falta email' });
-  if (!password) return res.status(400).json({ error: 'Falta password' });
+    try {
+        if (!email || !password) {
+            return res.status(400).json({ error: 'Falta email o contraseña' });
+        }
 
-  const idPeople = await loginPeopleService(email, password);
-  if (!idPeople) {
-    return res.status(400).json({ error: 'email o contraseña incorrectos' });
-  }
-  const data = await getPeopleService({ idPeople });
-  return res.status(200).json(data);
+        const authenticatedPerson = await authenticateUser(email, password);
+
+        if (!authenticatedPerson) {
+            return res.status(401).json({ error: 'Usuario o contraseña incorrectos' });
+        }
+
+        const idPeople = await loginPeopleService(authenticatedPerson);
+
+        const data = await getPeopleService({ idPeople });
+        return res.status(200).json(data);
+    } catch (error) {
+        if (error.message === 'Contraseña incorrecta') {
+            return res.status(401).json({ error: 'Usuario o contraseña incorrectos' });
+        } else {
+            return res.status(500).json({ error: 'Error interno del servidor' });
+        }
+    }
 };
 
 module.exports = loginPeopleController;
