bf2eac00ccae39951ab859b400319923bbb18a0d
Emmanuel <manumtz20@gmail.com>
2024-02-10 13:49:59 -0600

    ruta post categories option

--- src/routes/categories.route.js
+++ src/routes/categories.route.js
@@ -4,6 +4,7 @@ const { Router } = require("express");
 const getCategories = require("../controllers/categories/getCategories.controller");
 const getCategoriesOptions = require("../controllers/categories_options/getCategoriesOptions.controller");
 const postCategories = require("../controllers/categories/postCategories.controller");
+const postCategoriesOptionsController = require("../controllers/categories_options/postCategoriesOptions.controller");
 
 const categoriesRouter = Router();
 
@@ -67,5 +68,7 @@ categoriesRouter.post("/categories", postCategories);
 //  *       500:
 //  *         description: Error interno del servidor.
 //  */
+
+categoriesRouter.post("/categories/options", postCategoriesOptionsController);
 
 module.exports = categoriesRouter;
