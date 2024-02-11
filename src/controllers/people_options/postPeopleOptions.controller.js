cf4a69ad907c10d1349450802a3341c5892a1807
tiago1820 <tiago.zdo@gmail.com>
2024-02-11 13:40:09 -0300

    Fue documentada en Swagger la ruta POST '/people/options'

--- src/controllers/people_options/postPeopleOptions.controller.js
+++ src/controllers/people_options/postPeopleOptions.controller.js
@@ -1,14 +1,14 @@
 const postPeopleOptionsService = require('../../services/people_options/postPeopleOptions.service');
 
 const postPeopleOptionsController = async (req, res) => {
-  try {
-    const dataBody = req.body;
+    try {
+        const dataBody = req.body;
 
-    const { status, response } = await postPeopleOptionsService(dataBody);
-    res.status(status).json({ response });
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
+        const { status, response } = await postPeopleOptionsService(dataBody);
+        res.status(status).json({ response });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
 };
 
 module.exports = postPeopleOptionsController;
