fed2e46576457558273e9d2d687517db1a3cdafe
Emmanuel <manumtz20@gmail.com>
2024-02-13 00:56:06 -0600

    manejar conflictos en post questions

--- src/controllers/questions/postQuestions.controller.js
+++ src/controllers/questions/postQuestions.controller.js
@@ -1,5 +1,5 @@
-const { ValidationsError, ServerError } = require('../../errors');
-const postQuestionsService = require('../../services/questions/postQuestionsForm.service');
+const { ValidationsError, ServerError, ConflictError } = require('../../errors');
+const postQuestionsService = require('../../services/questions/postQuestions.service');
 
 const postQuestionsController = async (req, res) => {
   // const {
@@ -32,6 +32,10 @@ const postQuestionsController = async (req, res) => {
   } catch (error) {
     if (error instanceof ValidationsError) {
       return res.status(404).json({ error: error.message });
+    }
+
+    if (error instanceof ConflictError) {
+      return res.status(409).json({ error: error.message });
     }
 
     if (error instanceof ServerError) {
---
+++ src/services/questions/postQuestions.service.js
@@ -0,0 +1,129 @@
+const { TYPE_OF_QUESTION_QAA, TYPE_OF_QUESTION_FAQ } = require('../../constants');
+const { Questions } = require('../../db');
+const { v4: uuidv4 } = require('uuid'); // Importa la función para generar UUIDs
+const validationDataQAA = require('../../utils/validationDataQAA');
+const validationDataFAQ = require('../../utils/validationDataFAQ');
+const { ServerError, ValidationsError, ConflictError } = require('../../errors');
+
+const postQuestionsService = async (questionsData) => {
+  const {
+    typeOfQuestion,
+    destination,
+    priority,
+    senderMail,
+    fullName,
+    title,
+    receiverMail,
+    message,
+    response,
+  } = questionsData;
+
+  if (!typeOfQuestion) throw new ValidationsError('Tipo de pregunta es requerido');
+
+  const idQuestion = uuidv4();
+
+  if (typeOfQuestion === TYPE_OF_QUESTION_QAA) {
+    const newQuestionQAA = {
+      idQuestion,
+      typeOfQuestion,
+      destination,
+      receiverMail,
+      fullName,
+      senderMail,
+      title,
+      message,
+      response,
+    };
+
+    validationDataQAA(newQuestionQAA);
+
+    const [question, create] = await Questions.findOrCreate({
+      where: { title, message },
+      defaults: { newQuestionQAA },
+    });
+
+    if (!create) throw new ConflictError('La pregunta ya esta en curso');
+    if (!question) throw new ServerError('Error al crear nueva pregunta');
+
+    return { response: 'QAA creada exitosamente' };
+  }
+
+  if (typeOfQuestion === TYPE_OF_QUESTION_FAQ) {
+    const newQuestionFAQ = {
+      idQuestion,
+      typeOfQuestion,
+      destination,
+      priority,
+      title,
+      message,
+    };
+
+    validationDataFAQ(newQuestionFAQ);
+
+    const [question, create] = await Questions.findOrCreate({
+      where: { title, message },
+      defaults: { newQuestionFAQ },
+    });
+
+    if (!create) throw new ConflictError('La pregunta ya fue creada');
+    if (!question) throw new ServerError('Error al crear nueva pregunta');
+
+    const allQuestionsFAQ = await Questions.findAll({
+      where: { typeOfQuestion: TYPE_OF_QUESTION_FAQ },
+      attributes: {
+        exclude: ['response', 'priority', 'senderMail', 'fullName', 'receiverMail'],
+      },
+    });
+
+    const questions = {
+      count: allQuestionsFAQ.length,
+      filter: { typeOfQuestion: TYPE_OF_QUESTION_FAQ },
+      data: allQuestionsFAQ,
+    };
+
+    return { questions };
+  }
+
+  return { response: 'El tipo de pregunta no esta definida' };
+  //! reclamos y consultas:
+  // tipo de cuestion TYPE_OF_QUESTION_QAA *
+  // destinatario administrador *
+  // receiverMail
+  // nombre *
+  // senderMail *
+  // titulo *
+  // mensage *
+  // respuesta pendiente
+
+  //! FAQs
+  // tipo de cuestion TYPE_OF_QUESTION_FAQ *
+  // destinatario cliente o pprovedor o usuario externo *
+  // titulo *
+  // mensaje *
+
+  //   try {
+  //     // Genera un UUID para idQuestion
+  //     const idQuestion = uuidv4();
+
+  //     // Crea la pregunta con el UUID generado
+  //     const newQuestion = await Questions.create({
+  //       idQuestion, // Asigna el UUID generado
+  //       typeOfQuestion,
+  //       destination,
+  //       priority,
+  //       senderMail,
+  //       fullName,
+  //       title,
+  //       receiverMail,
+  //       message,
+  //       response,
+  //     });
+
+  //     return newQuestion;
+  //   } catch (error) {
+  //     console.error('Error creating question:', error);
+  //     throw new Error('Internal server error');
+  //   }
+};
+
+module.exports = postQuestionsService;
