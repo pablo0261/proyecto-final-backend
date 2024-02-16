b7df9dccec2cb3981abd3d42ac774cf4916a1a88
Emmanuel <manumtz20@gmail.com>
2024-02-16 17:32:55 -0600

    get categories options filtro para opciones eliminadas

--- src/services/categories_options/getCategoriesOption.service.js
+++ src/services/categories_options/getCategoriesOption.service.js
@@ -1,28 +1,43 @@
 const { Categories_options, Categories } = require('../../db');
 const { Op } = require('sequelize');
+const { ValidationsError, notFoundError } = require('../../errors');
 
 const getCategoriesOption = async (parameters = {}) => {
   //
   // array de filtros
-  const filters = [];
+  const categoryFilters = [];
+  const optionFilters = {};
+
   Object.entries(parameters).map(([key, value]) => {
     const obj = {};
     if (value) {
-      if (key === 'description') return filters.push({ description: { [Op.iLike]: value } });
-      obj[key] = value;
-      return filters.push(obj);
+      if (key === 'description') return categoryFilters.push({ description: { [Op.iLike]: value } });
+      if (key !== 'isDeleted') {
+        obj[key] = value;
+        return categoryFilters.push(obj);
+      }
     }
   });
 
+  // filtro por opciones borradas
+  let { isDeleted } = parameters;
+  if (isDeleted) {
+    if (isDeleted === 'false') isDeleted = false;
+    if (isDeleted === 'true') isDeleted = true;
+    if (typeof isDeleted !== 'boolean') throw new ValidationsError('isDeleted debe ser true o false');
+    optionFilters.isDeleted = isDeleted;
+  }
+
   const categoriesOptionsData = await Categories_options.findAll({
+    where: optionFilters,
     include: {
       model: Categories,
-      where: { [Op.and]: filters },
+      where: { [Op.and]: categoryFilters },
       attributes: ['description'],
     },
   });
 
-  if (!categoriesOptionsData[0]) return { status: 404, response: 'Sin ningun resultado' };
+  if (!categoriesOptionsData[0]) throw new notFoundError('Sin ningun resultado');
 
   const count = categoriesOptionsData.length;
   const categoriesOptions = {
@@ -30,7 +45,7 @@ const getCategoriesOption = async (parameters = {}) => {
     filters: parameters,
     data: categoriesOptionsData,
   };
-  return { status: 200, response: { categoriesOptions } };
+  return { categoriesOptions };
 };
 
 module.exports = getCategoriesOption;
