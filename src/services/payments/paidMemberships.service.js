5b42d2675ed06ead563f89d8b4bdcc6fc386f858
tiago1820 <tiago.zdo@gmail.com>
2024-02-18 13:20:23 -0300

    Se filtra informacion de pagos por idPeople

--- src/services/payments/paidMemberships.service.js
+++ src/services/payments/paidMemberships.service.js
@@ -3,46 +3,39 @@ const { Op } = require('sequelize');
 
 const paidMembershipsService = async (filter) => {
 
+    console.log("idPeople: ", filter.idPeople);
     try {
+
+        const whereCondition = filter.idPeople ? { paymentDay: null, idPeople: filter.idPeople } : { paymentDay: null };
+
         const countNullPaymentDay = await Payments.count({
-            where: {
-                paymentDay: null
-            }
+            where: whereCondition
         });
 
         const payment = await Payments.findOne({
             attributes: ['price'],
-            where: {
-                paymentDay: null
-            }
+            where: whereCondition
         });
 
         const fixedPrice = payment ? payment.price : 0;
-        const countTimesPrice = countNullPaymentDay * fixedPrice;
+
+        const total = countNullPaymentDay * fixedPrice;
 
         const nullPaymentDayRecords = await Payments.findAll({
-            where: {
-                paymentDay: null
-            }
+            where: whereCondition
         });
-
-
 
         const info = {
             count: countNullPaymentDay,
-            total: countTimesPrice,
+            total: total,
             data: nullPaymentDayRecords
-        }
+        };
 
         return info;
-
-
     } catch (error) {
         console.log("service: ", error);
         throw error;
     }
-
 }
-
 
 module.exports = { paidMembershipsService };
